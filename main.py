32209ed83bd3e353f6a32e0d0fde8b94a0515eab
Ruslan Shafikov <rs31113@yandex.ru>
2023-02-22 17:50:48 +0300

    deleted csv and excel convertation to get rid of opencv

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from PIL import Image
 from docx import Document
 import os
 from pytube import YouTube
-import pandas as pd
 import moviepy.editor as moviepy
 from config import TOKEN
 from pdf2docx import parse
@@ -98,24 +97,6 @@ async def convert_docx(callback_query: types.CallbackQuery):
 async def convert_pdf(callback_query: types.CallbackQuery):
     path = 'storage/test.docx'
     convert_pdf2docx('storage/test.pdf', path)
-    await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
-    clear_storage()
-
-
-@dp.callback_query_handler(text=['csv xlsx'])
-async def convert_csv(callback_query: types.CallbackQuery):
-    path = 'storage/test.xlsx'
-    read_file = pd.read_csv('storage/test.csv')
-    read_file.to_excel(path)
-    await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
-    clear_storage()
-
-
-@dp.callback_query_handler(text=['xlsx csv'])
-async def convert_xlsx(callback_query: types.CallbackQuery):
-    path = 'storage/test.csv'
-    read_file = pd.read_excel("storage/test.xlsx")
-    read_file.to_csv(path)
     await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
     clear_storage()
 
@@ -248,13 +229,7 @@ async def file_processing(message: types.Message):
     valid = True
     extension = message.document.file_name.split(".")[1].lower()
     kb = InlineKeyboardMarkup(row_width=2)
-    if extension == "xlsx":
-        btn_1 = InlineKeyboardButton('CSV', callback_data='xlsx csv')
-        kb.add(btn_1)
-    elif extension == "csv":
-        btn_1 = InlineKeyboardButton('XLSX', callback_data='csv xlsx')
-        kb.add(btn_1)
-    elif extension == "heic":
+    if extension == "heic":
         kb.add(png_btn, jpg_btn, jpeg_btn, pdf_btn, bmp_btn)
     elif extension == "docx":
         btn_1 = InlineKeyboardButton('PDF', callback_data='docx pdf')
