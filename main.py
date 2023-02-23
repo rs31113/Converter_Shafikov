709401b67310e70e10d62c86b223a6df3e93a3d1
Ruslan Shafikov <rs31113@yandex.ru>
2023-02-23 14:35:08 +0300

    file storage system changed.

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ from heic2png import HEIC2PNG
 from pdf2docx import parse
 
 
-def clear_storage():
-    for file in os.listdir("storage"):
-        os.remove(f'storage/{file}')
+def clear_storage(chat_id):
+    for file in os.listdir(f"storage/{chat_id}"):
+        os.remove(f'storage/{chat_id}/{file}')
 
 
 def convert_pdf2docx(input_file: str, output_file: str, pages: Tuple = None):
@@ -29,7 +29,7 @@ def convert_pdf2docx(input_file: str, output_file: str, pages: Tuple = None):
 def download_from_youtube(youtube_link):
     video = YouTube(youtube_link)
     video = video.streams.get_highest_resolution()
-    video.download('storage', 'test.mp4')
+    video.download('storage', f'{chat_id}', 'test.mp4')
 
 
 async def reply_to_user(extension, kb, message, valid=True):
@@ -56,9 +56,10 @@ photo_btn = InlineKeyboardButton('PHOTO', callback_data='photo')
 
 @dp.callback_query_handler(text=['txt text', 'txt docx'])
 async def convert_txt(callback_query: types.CallbackQuery):
+    chat_id = callback_query.message['chat']['id']
     convert_to = callback_query.data.split()[1]
-    path = 'storage/test.docx'
-    f = open("storage/test.txt", "r")
+    path = f'storage/{chat_id}/test.docx'
+    f = open(f"storage/{chat_id}/test.txt", "r")
     f = f.readlines()
     heading = f[0]
     f.pop(0)
@@ -71,15 +72,16 @@ async def convert_txt(callback_query: types.CallbackQuery):
         document.add_paragraph(text)
         document.save(path)
         await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
-    clear_storage()
+    clear_storage(chat_id)
 
 
 @dp.callback_query_handler(text=['docx pdf', 'docx txt', 'docx text'])
 async def convert_docx(callback_query: types.CallbackQuery):
     convert_to = callback_query.data.split()[1]
-    path = f'storage/test.{convert_to}'
+    chat_id = callback_query.message['chat']['id']
+    path = f'storage/{chat_id}/test.{convert_to}'
     if convert_to == 'text' or convert_to == 'txt':
-        doc = Document(f"storage/test.docx")
+        doc = Document(f"storage/{chat_id}/test.docx")
         result = [p.text for p in doc.paragraphs]
         result = '\n'.join(result)
         if convert_to == 'text':
@@ -90,99 +92,107 @@ async def convert_docx(callback_query: types.CallbackQuery):
             f.close()
             await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
     else:
-        convert('storage/test.docx', 'storage/test.pdf')
-    clear_storage()
+        convert(f'storage/{chat_id}/test.docx', f'storage/{chat_id}/test.pdf')
+    clear_storage(chat_id)
 
 
 @dp.callback_query_handler(text=['pdf docx'])
 async def convert_pdf(callback_query: types.CallbackQuery):
-    path = 'storage/test.docx'
-    convert_pdf2docx('storage/test.pdf', path)
+    chat_id = callback_query.message['chat']['id']
+    path = f'storage/{chat_id}/test.docx'
+    convert_pdf2docx(f'storage/{chat_id}/test.pdf', path)
     await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
-    clear_storage()
+    clear_storage(chat_id)
 
 
 @dp.callback_query_handler(text=['csv xlsx'])
 async def convert_csv(callback_query: types.CallbackQuery):
-    path = 'storage/test.xlsx'
-    read_file = pd.read_csv('storage/test.csv')
+    chat_id = callback_query.message['chat']['id']
+    path = f'storage/{chat_id}/test.xlsx'
+    read_file = pd.read_csv(f'storage/{chat_id}/test.csv')
     read_file.to_excel(path)
     await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
-    clear_storage()
+    clear_storage(chat_id)
 
 
 @dp.callback_query_handler(text=['xlsx csv'])
 async def convert_xlsx(callback_query: types.CallbackQuery):
-    path = 'storage/test.csv'
-    read_file = pd.read_excel("storage/test.xlsx")
+    chat_id = callback_query.message['chat']['id']
+    path = f'storage/{chat_id}/test.csv'
+    read_file = pd.read_excel(f"storage/{chat_id}/test.xlsx")
     read_file.to_csv(path)
     await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
-    clear_storage()
+    clear_storage(chat_id)
 
 
 @dp.callback_query_handler(text=['video mp3'])
 async def convert_video(callback_query: types.CallbackQuery):
-    video = moviepy.VideoFileClip('storage/test.mp4')
+    chat_id = callback_query.message['chat']['id']
+    video = moviepy.VideoFileClip(f'storage/{chat_id}/test.mp4')
     audio = video.audio
-    path = 'storage/test.mp3'
+    path = f'storage/{chat_id}/test.mp3'
     audio.write_audiofile(path)
     audio.close()
     video.close()
     await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
-    clear_storage()
+    clear_storage(chat_id)
 
 
 @dp.callback_query_handler(text=['mov', 'avi'])
 async def convert_to_mp4(callback_query: types.CallbackQuery):
+    chat_id = callback_query.message['chat']['id']
     convert_to = callback_query.data.split()[0]
-    path = 'storage/test.mp4'
-    video = moviepy.VideoFileClip(f'storage/test.{convert_to}')
+    path = f'storage/{chat_id}/test.mp4'
+    video = moviepy.VideoFileClip(f'storage/{chat_id}/test.{convert_to}')
     video.write_videofile(path)
     video.close()
     await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
+    clear_storage(chat_id)
 
 
 @dp.callback_query_handler(text=['text txt', 'text docx'])
 async def convert_text(callback_query: types.CallbackQuery):
+    chat_id = callback_query.message['chat']['id']
     convert_to = callback_query.data.split()[1]
-    path = f'storage/test.{convert_to}'
+    path = f'storage/{chat_id}/test.{convert_to}'
     if convert_to == "txt":
         await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
     else:
         document = Document()
-        with open('storage/test.txt') as f:
+        with open(f'storage/{chat_id}/test.txt') as f:
             for line in f:
                 document.add_paragraph(line)
         document.save(path)
         await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
-    clear_storage()
+    clear_storage(chat_id)
 
 
 @dp.callback_query_handler(text=['png', 'pdf', 'bmp', 'jpeg', 'jpg', 'photo'])
 async def convert_photo(callback_query: types.CallbackQuery):
-    convert_from = os.listdir('storage')[0].split('.')[1]
+    chat_id = callback_query.message['chat']['id']
+    convert_from = os.listdir(f'storage/{chat_id}')[0].split('.')[1]
     convert_to = callback_query.data.split()[0]
     if convert_to == 'photo':
-        chat_id = callback_query.message['chat']['id']
-        photo = open(f'storage/test.{convert_from}', 'rb')
+        photo = open(f'storage/{chat_id}/test.{convert_from}', 'rb')
         await bot.send_photo(chat_id=chat_id, photo=photo, caption=bot_link, parse_mode=mode)
     else:
-        path = f"storage/test.{convert_to}"
+        path = f"storage/{chat_id}/test.{convert_to}"
         if convert_from == 'heic':
-            file = HEIC2PNG('storage/test.heic')
+            file = HEIC2PNG(f'storage/{chat_id}/test.heic')
             file.save()
             convert_from = 'png'
         await bot.answer_callback_query(callback_query.id)
-        image_1 = Image.open(f'storage/test.{convert_from}')
+        image_1 = Image.open(f'storage/{chat_id}/test.{convert_from}')
         im_1 = image_1.convert('RGB')
         im_1.save(path)
         await callback_query.message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
-    clear_storage()
+    clear_storage(chat_id)
 
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    clear_storage()
+    chat_id = message.chat.id
+    os.mkdir(f'storage/{chat_id}')
     await message.answer("Привет! Конвертер файлов к "
                          "вашим услугам!\n\nЯ могу изменить "
                          "формат вашего файла.\nПросто отправь мне любой файл, а я "
@@ -191,43 +201,47 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler(content_types=['voice'])
 async def voice_processing(message: types.Message):
-    path = 'storage/test.mp3'
+    chat_id = message['chat']['id']
+    path = f'storage/{chat_id}/test.mp3'
     await message.voice.download(destination_file=path)
     await message.answer_document(InputFile(path), caption=bot_link, parse_mode=mode)
-    clear_storage()
+    clear_storage(chat_id)
 
 
 @dp.message_handler(content_types=['photo'])
 async def photo_processing(message: types.Message):
+    chat_id = message['chat']['id']
     kb = InlineKeyboardMarkup(row_width=2)
     kb.add(jpg_btn, jpeg_btn, png_btn, pdf_btn, bmp_btn)
     extension = "photo"
-    await message.photo[-1].download(destination_file='storage/test.jpg')
+    await message.photo[-1].download(destination_file=f'storage/{chat_id}/test.jpg')
     await reply_to_user(extension, kb, message)
 
 
 @dp.message_handler(content_types=['sticker'])
 async def sticker_processing(message: types.Message):
+    chat_id = message['chat']['id']
     kb = InlineKeyboardMarkup(row_width=2)
     extension = "STICKER"
     kb.add(jpg_btn, jpeg_btn, png_btn, pdf_btn, bmp_btn, photo_btn)
-    await message.sticker.download(destination_file='storage/test.jpg')
+    await message.sticker.download(destination_file=f'storage/{chat_id}/test.jpg')
     await reply_to_user(extension, kb, message)
 
 
 @dp.message_handler(content_types=['text'])
 async def text_processing(message: types.Message):
+    chat_id = message['chat']['id']
     try:
         download_from_youtube(message.text)
-        await message.answer_document(InputFile('storage/test.mp4'), caption=bot_link, parse_mode=mode)
-        clear_storage()
+        await message.answer_document(InputFile(f'storage/{chat_id}/test.mp4'), caption=bot_link, parse_mode=mode)
+        clear_storage(chat_id)
     except:
         kb = InlineKeyboardMarkup(row_width=2)
         btn_1 = InlineKeyboardButton('TXT', callback_data='text txt')
         btn_2 = InlineKeyboardButton('DOCX', callback_data='text docx')
         kb.add(btn_1, btn_2)
         extension = "text"
-        file = open('storage/test.txt', 'w+')
+        file = open(f'storage/{chat_id}/test.txt', 'w+')
         file.write(message.text)
         file.close()
         await reply_to_user(extension, kb, message)
@@ -235,16 +249,18 @@ async def text_processing(message: types.Message):
 
 @dp.message_handler(content_types=['video'])
 async def video_processing(message: types.Message):
+    chat_id = message['chat']['id']
     kb = InlineKeyboardMarkup(row_width=2)
     btn_1 = InlineKeyboardButton('MP3', callback_data='video mp3')
     kb.add(btn_1)
     extension = "mp4"
-    await message.video.download(destination_file='storage/test.mp4')
+    await message.video.download(destination_file=f'storage/{chat_id}/test.mp4')
     await reply_to_user(extension, kb, message)
 
 
 @dp.message_handler(content_types=['document'])
 async def file_processing(message: types.Message):
+    chat_id = message['chat']['id']
     valid = True
     extension = message.document.file_name.split(".")[1].lower()
     kb = InlineKeyboardMarkup(row_width=2)
@@ -282,7 +298,7 @@ async def file_processing(message: types.Message):
     else:
         valid = False
     if valid:
-        await message.document.download(destination_file=f"storage/test.{extension}")
+        await message.document.download(destination_file=f"storage/{chat_id}/test.{extension}")
     await reply_to_user(extension, kb, message, valid)
 
 
